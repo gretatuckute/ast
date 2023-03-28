3d7eecc15dfa36242f643973d4f4b72c941149b7
Greta Tuckute <gretatu@mit.edu>
2023-03-28 09:25:31 -0400

    run permuted network

--- src/run_model.py
+++ src/run_model.py
@@ -12,6 +12,8 @@ import random
 
 ## SETTINGS ##
 RESULTDIR = '/Users/gt/Documents/GitHub/aud-dnn/aud_dnn/model-actv/AST/'
+if not os.path.exists(RESULTDIR):
+	os.makedirs(RESULTDIR)
 DATADIR = '/Users/gt/Documents/GitHub/aud-dnn/data/stimuli/165_natural_sounds_16kHz/'
 
 np.random.seed(0)
@@ -19,8 +21,9 @@ random.seed(0)
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
-run_only_missing_files = False
-rand_netw = False
+get_sound_stats = False # compute mean and std of the dataset of interest (165 sounds)
+run_only_missing_files = True
+rand_netw = True # if True, run the model with permuted weights
 
 files = [f for f in os.listdir(DATADIR) if os.path.isfile(os.path.join(DATADIR, f))]
 wav_files_identifiers = [f for f in files if f.endswith('wav')]
@@ -37,6 +40,9 @@ if run_only_missing_files:
 	existing_actv = [f for f in os.listdir(RESULTDIR) if os.path.isfile(os.path.join(RESULTDIR, f))]
 	set_files = set(identifier_pkls) - set(existing_actv)
 	wav_files_paths = [DATADIR + f.split('_activations')[0] + '.wav' for f in set_files]
+
+
+
 
 def make_features(wav_name, mel_bins, target_length=1024):
 	"""Copied over from inference.py
@@ -64,6 +70,113 @@ def make_features(wav_name, mel_bins, target_length=1024):
 	
 	return fbank
 
+
+def get_sound_stats(wav_name, mel_bins, target_length=1024, new_rms=None, new_rms_scale=None):
+	"""Copy of make_features
+
+	If new_rms is not None, then the sound is scaled to have the new_rms amplitude.
+	If new_rms_scale is not None, then the sound is just scaled by the new_rms_scale.
+
+	"""
+
+	waveform, sr = torchaudio.load(wav_name)
+	# rms_predemean = ch_rms(waveform)
+	waveform = ch_demean(waveform)
+
+
+	# print RMS
+	# rms = ch_rms(waveform)
+	# print(f'RMS pre demean: {rms_predemean} and post demean: {rms}')
+
+	if new_rms is not None:
+		waveform = waveform / ch_rms(waveform) * new_rms
+
+	if new_rms_scale is not None:
+		waveform = waveform * new_rms_scale
+
+	fbank = torchaudio.compliance.kaldi.fbank(
+		waveform, htk_compat=True, sample_frequency=sr, use_energy=False,
+		window_type='hanning', num_mel_bins=mel_bins, dither=0.0,
+		frame_shift=10)
+
+	n_frames = fbank.shape[0]
+
+	p = target_length - n_frames  # check for target length, for the audioset trained model it is 1024 frames
+	if p > 0:
+		m = torch.nn.ZeroPad2d((0, 0, 0, p))
+		fbank = m(fbank)
+	elif p < 0:
+		fbank = fbank[0:target_length, :]
+
+	# fbank = (fbank - (-4.2677393)) / (4.5689974 * 2)
+
+	# plt.imshow(fbank, interpolation=None)
+	# plt.show()
+
+	return fbank
+
+
+def ch_demean(x, dim=-1):
+	'''
+    Helper function to mean-subtract tensor.
+
+    Args
+    ----
+    x (tensor): tensor to be mean-subtracted
+    dim (int): kwarg for torch.mean (dim along which to compute mean)
+
+    Returns
+    -------
+    x_demean (tensor): mean-subtracted tensor
+    '''
+	x_demean = torch.sub(x, torch.mean(x, dim=dim))
+	return x_demean
+
+
+def ch_rms(x, dim=-1):
+	'''
+    Helper function to compute RMS amplitude of a tensor.
+
+    Args
+    ----
+    x (tensor): tensor for which RMS amplitude should be computed
+    dim (int): kwarg for torch.mean (dim along which to compute mean)
+
+    Returns
+    -------
+    rms_x (tensor): root-mean-square amplitude of x
+    '''
+	rms_x = torch.sqrt(torch.mean(torch.pow(x, 2), dim=dim))
+	return rms_x
+
+
+
+if get_sound_stats:
+	lst_fbanks = []
+	for wav_file in tqdm(wav_files_paths):
+		fbank = get_sound_stats(wav_name=wav_file,
+								mel_bins=128,
+								target_length=1024,
+								new_rms=None,
+								new_rms_scale=10) # 10 and 0.1
+
+		lst_fbanks.append(fbank)
+
+	# Compute the mean and std of the dataset
+	mean_fbank = torch.mean(torch.stack(lst_fbanks), dim=0)
+	mean_dataset = mean_fbank.mean()
+	std_dataset = mean_fbank.std()
+
+
+	print(f'Mean of the dataset: {mean_dataset:3f} and std of the dataset: {std_dataset:3f}')
+
+
+
+
+
+
+
+
 # download pretrained model in this directory
 os.environ['TORCH_HOME'] = '../pretrained_models'
 
@@ -81,16 +194,20 @@ if rand_netw:
 	print('OBS! RANDOM NETWORK!')
 
 	## The following code was used to generate indices for random permutation ##
-	# d_rand_idx = {}  # create dict for storing the indices for random permutation
-	# for k, v in state_dict.items():
-	# 	w = state_dict[k]
-	# 	idx = torch.randperm(w.nelement())  # create random indices across all dimensions
-	# 	d_rand_idx[k] = idx
-	#
-	# with open(os.path.join(os.getcwd(), 'AST_randnetw_indices.pkl'), 'wb') as f:
-	# 	pickle.dump(d_rand_idx, f)
-	
-	d_rand_idx = pickle.load(open(os.path.join(os.getcwd(), 'AST_randnetw_indices.pkl'), 'rb'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'AST_randnetw_indices.pkl')):
+		print('________ Generating random indices for permuted architecture ________')
+		d_rand_idx = {}  # create dict for storing the indices for random permutation
+		for k, v in state_dict.items():
+			w = state_dict[k]
+			idx = torch.randperm(w.nelement())  # create random indices across all dimensions
+			d_rand_idx[k] = idx
+
+		with open(os.path.join(os.getcwd(), 'AST_randnetw_indices.pkl'), 'wb') as f:
+			pickle.dump(d_rand_idx, f)
+
+	else:
+		print('________ Loading random indices from permuted architecture ________')
+		d_rand_idx = pickle.load(open(os.path.join(os.getcwd(), 'AST_randnetw_indices.pkl'), 'rb'))
 	
 	for k, v in state_dict.items():
 		w = state_dict[k]
@@ -103,16 +220,10 @@ if rand_netw:
 	model.load_state_dict(state_dict_rand)
 
 ### LOOP OVER AUDIO FILES ###
-lst_mean = []
-lst_std = []
 for filename in tqdm(wav_files_paths):
 	
 	input = make_features(filename, mel_bins=128, target_length=1024)
-	lst_mean.append(input)
 
-# print('\n\n\n')
-# np.mean([x.mean(axis=0).detach().numpy() for x in lst_mean])
-	
 	# add a batch dim
 	input = input[None, :, :]
 
@@ -154,4 +265,4 @@ for filename in tqdm(wav_files_paths):
 	id1 = filename.split('/')[-1]
 	identifier = id1.split('.')[0]
 
-	# save_output.store_activations(RESULTDIR=RESULTDIR, identifier=identifier)
+	save_output.store_activations(RESULTDIR=RESULTDIR, identifier=identifier)
